ae8eb15d32adbc4235bd62c6def1c160a0a25a22
peter <withtvpeter@gmail.com>
2018-10-05 09:39:36 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,45 @@
 $(document).ready(function(){
 
 // call functions here
+frameIt();
+
+getIt();
+pressIt();
 
 });
+
+function getIt()
+{
+  $('p:first-child').on('click', function(){
+    return alert('Hey!');
+  });
+}
+
+function frameIt()
+{
+return $("img:first").on("load", function(){
+    $(this).addClass("tasty");
+  });
+    
+}
+
+function pressIt()
+{
+  $("form").on('keydown', function(key){
+    
+    if( $("input:first").val() === "g"){
+      alert("you pressed g letter");
+      return;
+    }
+    
+  });
+}
+
+function submitIt()
+{
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.");
+    return;
+  })
+}
+

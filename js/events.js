1c57897f26934d6f740a2cdaf6d260905ca3873d
peter <withtvpeter@gmail.com>
2018-10-05 10:17:28 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,45 @@
 $(document).ready(function(){
 
 // call functions here
+frameIt();
+
+getIt();
+pressIt();
 
 });
+
+function getIt()
+{
+  $('p:first-child').on('click', function(){
+    return alert('Hey!');
+  });
+}
+
+function frameIt()
+{
+return $("img:first").on("load", function(){
+    $(this).addClass("tasty");
+  });
+    
+}
+
+function pressIt()
+{
+  $("input:first").on('keydown', function(key){
+    
+ if( key.which  === 71)
+ {
+   alert ("GKEY HAS BEEN PRESSEd")
+ }
+    
+  });
+}
+
+function submitIt()
+{
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.");
+    return;
+  })
+}
+
